e7c49b5b0ed38df3e17ca8bd6ef720ce2f616228
Martin Seeger <mjseeger@gmail.com>
2021-03-22 23:24:46 +0100

    Started unit tests MRNA

--- analysis/TRSL2_setup.py
+++ analysis/TRSL2_setup.py
@@ -1,6 +1,6 @@
-# from translation import parameters
 from translation import MRNA, parameters
 
 testMRNA = MRNA.MRNA(index=0, length=parameters.mRNA_av_length, geneID=None, ribosomes={})
+print(testMRNA.ribosomes)
 testMRNA.attach_ribosome_at_start()
-
+print(testMRNA.ribosomes)
---
+++ test/unittests_MRNA.py
@@ -0,0 +1,14 @@
+import unittest
+from translation import MRNA, parameters
+
+
+class TestMRNAMethods(unittest.TestCase):
+
+    def test_attach_ribosome_at_start(self):
+        testMRNA = MRNA.MRNA(index=0, length=parameters.mRNA_av_length, geneID=None, ribosomes={})
+        testMRNA.attach_ribosome_at_start()
+        self.assertEqual(testMRNA.ribosomes, {0: None})
+
+
+if __name__ == '__main__':
+    unittest.main()
