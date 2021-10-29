8f3cf2c736594bf128bf2377336786487562b1fc
gittenberg <mjseeger@gmail.com>
2021-10-29 08:50:20 +0200

    Added test_elongate_one_step

--- test/unittests_TRSL.py
+++ test/unittests_TRSL.py
@@ -2,6 +2,7 @@ import unittest
 import collections as col
 from translation import MRNA, TRSL, parameters
 
+
 class TestTRSLMethods(unittest.TestCase):
     def test_insert_tRNA(self):
         trsl = TRSL.TRSL(nribo=2, proteome=col.Counter({}), types_tRNA=2, n_mRNA=2, detail=False)
@@ -34,6 +35,21 @@ class TestTRSLMethods(unittest.TestCase):
     # not tested because stochastic outcome
     def test_fill_empty_ribosomes(self):
         self.assertTrue(True)
+
+    def test_elongate_one_step(self):
+        trsl = TRSL.TRSL(nribo=2, proteome=col.Counter({}), types_tRNA=2, n_mRNA=2, detail=False)
+        # create mRNAs with unoccupied ribosomes
+        trsl.mRNAs = [MRNA.MRNA(index=gene, length=66, ribosomes={3: None}) for gene in [0, 1]]
+        # now insert tRNA into one of the mRNAs
+        _ = trsl.insert_tRNA(mRNA=trsl.mRNAs[0], pos=3, tRNA_type=1)
+        success1 = trsl.elongate_one_step(trsl.mRNAs[0], current_pos=3)
+        self.assertTrue(success1)
+        trsl.mRNAs = [MRNA.MRNA(index=gene, length=12, ribosomes={3: None}) for gene in [0, 1]]
+        # now insert tRNA into one of the mRNAs
+        _ = trsl.insert_tRNA(mRNA=trsl.mRNAs[0], pos=3, tRNA_type=1)
+        success2 = trsl.elongate_one_step(trsl.mRNAs[0], current_pos=3)
+        # False because mRNA is too short
+        self.assertFalse(success2)
 
 
 if __name__ == '__main__':
